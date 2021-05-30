b3a6b64a6e249e1973e157cd6a95f878689b185c
jac <helios.ftwang@gmail.com>
2021-05-30 01:28:00 -0400

    Add About222 blank pages for test

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 export const About = (props) => {
     return (
         <div>
-            <h1>This is about</h1>
+            <h1>This is about 222</h1>
         </div>
     );
 };
