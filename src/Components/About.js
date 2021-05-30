4e59bae866504fa3fa2237a34c6cfbfeac25ef58
jac <helios.ftwang@gmail.com>
2021-05-30 01:22:09 -0400

    Add About blank pages for test

---
+++ src/Components/About.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export const About = (props) => {
+    return (
+        <div>
+            <h1>This is about</h1>
+        </div>
+    );
+};
+
+export default About;
