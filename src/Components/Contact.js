f29e51e04410359adfaa6dddb338d9ebee529858
jac <helios.ftwang@gmail.com>
2021-05-30 01:04:52 -0400

    Add Home, contact blank pages for test

---
+++ src/Components/Contact.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export const Contact = (props) => {
+    return (
+        <div>
+            <h1>This is Contact Page.</h1>
+        </div>
+    );
+};
+
+export default Contact
---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+// @flow
+import * as React from 'react';
+
+export const Home = (props) => {
+    return (
+        <div>
+            <h1>This is Home page</h1>
+        </div>
+    );
+};
+
+export default Home
